1718bee807d13f2387833f94eae576d60a18e091
senchromatic <samuelfzhang@gmail.com>
2017-05-12 22:36:09 -0400

    rename identifiers -> video_ids

--- example.R
+++ example.R
@@ -1,10 +1,10 @@
 source("ranker.R")
 
 # Ballade No. 1 in G minor, Op. 23 - Frederic Chopin
-generate.rankings("identifiers/chopin_ballade_1.csv")
+generate.rankings("video_ids/chopin_ballade_1.csv")
 
 # La Marseillaise - Claude Joseph Rouget de Lisle
-generate.rankings("identifiers/la_marseillaise.csv")  
+generate.rankings("video_ids/la_marseillaise.csv")  
 
 # You Are My Sunshine - Jimmie Davis, Charles Mitchell
-generate.rankings("identifiers/you_are_my_sunshine.csv")
+generate.rankings("video_ids/you_are_my_sunshine.csv")
