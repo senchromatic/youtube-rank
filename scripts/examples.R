c210e1780e859f7e0232f607ac086b07078f1f18
senchromatic <samuelfzhang@gmail.com>
2017-05-18 21:27:29 -0400

    disable scientific notation, add ABBA and Vivaldi examples

--- scripts/examples.R
+++ scripts/examples.R
@@ -1,7 +1,10 @@
 # setwd("path/to/repository/root/directory/")
 source("scripts/ranker.R")
 
-# Ballade No. 1 in G minor, Op. 23 - Frederic Chopin
+# disable scientific notation
+options(scipen=99)
+
+# Ballade No. 1 in G minor, Op. 23 - Frédéric Chopin
 generate.rankings("video_ids/chopin_ballade_1.csv")
 
 # La Marseillaise - Claude Joseph Rouget de Lisle
@@ -9,3 +12,9 @@ generate.rankings("video_ids/la_marseillaise.csv")
 
 # You Are My Sunshine - Jimmie Davis, Charles Mitchell
 generate.rankings("video_ids/you_are_my_sunshine.csv")
+
+# ABBA discography (all songs)
+generate.rankings("video_ids/abba.csv")
+
+# The Four Seasons - Antonio Lucio Vivaldi
+generate.rankings("video_ids/vivaldi_le_quattro_stagioni.csv")
